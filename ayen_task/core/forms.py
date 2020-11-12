9b3bb8326db80cb75956f67ba397c8e99c42607c
Ahmed Shahwan <ahmed@shahwan.me>
2020-11-12 03:30:15 +0200

    Document Model and Form

--- ayen_task/core/forms.py
+++ ayen_task/core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from ayen_task.core.models import Document
 
 
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
+class DocumentForm(forms.ModelForm):
+    class Meta:
+        model = Document
+        fields = ("name", "file")
--- ayen_task/core/models.py
+++ ayen_task/core/models.py
@@ -1,3 +1,15 @@
-# from django.db import models
+import os
+from django.core.exceptions import ValidationError
+from django.db import models
 
-# Create your models here.
+
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]
+    valid_extensions = [".pdf", ".pptx"]
+    if ext.lower() not in valid_extensions:
+        raise ValidationError("Only `pdf` or `pptx` documents.")
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to="uploads", validators=[validate_file_extension])
