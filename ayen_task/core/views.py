c285e3d3c443d590523ac1ae14fbcc8f12e81f06
Ahmed Shahwan <ahmed@shahwan.me>
2020-11-13 03:26:16 +0200

    Search functionality

--- ayen_task/core/views.py
+++ ayen_task/core/views.py
@@ -1,5 +1,10 @@
+import os
+import pdftotext
+from pptx import Presentation
+
+from django.shortcuts import render
+from django.views.generic.base import View
 from django.views.generic.edit import CreateView
-from django.views.generic import FormView
 from django.views.generic.list import ListView
 
 from ayen_task.core.forms import SearchForm
@@ -11,9 +16,39 @@ class Home(ListView):
     template_name = "home.html"
 
 
-class Search(FormView):
-    form_class = SearchForm
-    template_name = "search.html"
+class Search(View):
+    def get(self, request, *args, **kwargs):
+        keyword = request.GET.get("keyword")
+        object_list = []
+
+        if keyword:
+            for document in Document.objects.all():
+                # searching file name
+                if keyword in document.name:
+                    object_list.append(document)
+                # searching file content
+                else:
+                    path = document.file.path
+                    extension = os.path.splitext(path)[1]
+
+                    if extension == ".pdf":
+                        with open(path, "rb") as f:
+                            pdf = pdftotext.PDF(f)
+                            for page in pdf:
+                                if keyword in page:
+                                    object_list.append(document)
+
+                    elif extension == ".pptx":
+                        presentation = Presentation(str(path))
+                        for slide in presentation.slides:
+                            for shape in slide.shapes:
+                                if hasattr(shape, "text"):
+                                    shape.text = shape.text.lower()
+                                    if keyword.lower() in shape.text:
+                                        object_list.append(document)
+
+        context = {"form": SearchForm(), "object_list": object_list}
+        return render(request, "search.html", context)
 
 
 class Upload(CreateView):
